b45b787a36910054ecb6617d8d1a469d60687a97
Seung-hae KIM <aka.seunghaekim@gmail.com>
2018-06-06 03:09:44 +0900

    MODIFY migration

--- bibliography/migrations/0003_book_images.py
+++ bibliography/migrations/0003_book_images.py
@@ -7,8 +7,9 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('bibliography', '0001_initial'),
         ('photo', '0002_auto_20180527_1636'),
-        ('bibliography', '0002_remove_book_images'),
+        # ('bibliography', '0002_remove_book_images'),
     ]
 
     operations = [
