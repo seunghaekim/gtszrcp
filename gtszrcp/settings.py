43f8925899983561cb02467fcb3c3e5af1c05170
seunghaekim <aka.seunghaekim@gmail.com>
2018-07-08 10:05:22 +0900

    try to find admin page

--- gtszrcp/settings.py
+++ gtszrcp/settings.py
@@ -114,15 +114,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Seoul'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
